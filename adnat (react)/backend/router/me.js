2f9b281f3e655477870ebe3e33ced4819ce599ea
Daniel Gilchrist <dan@tanda.co>
2019-02-11 16:40:56 +1000

    Added update endpoint

--- adnat (react)/backend/router/me.js
+++ adnat (react)/backend/router/me.js
@@ -7,4 +7,23 @@ router.use(sessionMiddleware);
 
 router.get("/", (req, res) => res.json(req.user));
 
+router.put("/update", (req, res) => {
+  const { name, email } = req.body;
+
+  return DB.run(
+    "UPDATE users SET name = ?, email = ? WHERE id = ?",
+    name || req.user.name,
+    email || req.user.email,
+    req.user.id
+  )
+    .then(() => res.sendStatus(200))
+    .catch(err => {
+      if (err && err.statusCode) {
+        return res.sendStatus(err.statusCode);
+      }
+
+      throw err;
+    });
+});
+
 module.exports = router;
