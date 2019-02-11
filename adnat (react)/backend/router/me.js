f23ff10ae0f477336c48ac85f44c26bbc4466883
Daniel Gilchrist <dan@tanda.co>
2019-02-11 16:42:00 +1000

    Added change password endpoint

--- adnat (react)/backend/router/me.js
+++ adnat (react)/backend/router/me.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const DB = require("../db");
+const { comparePassword, hashPassword } = require("../util/password");
 const { sessionMiddleware } = require("../util/session");
 
 router.use(sessionMiddleware);
@@ -22,6 +23,39 @@ router.put("/update", (req, res) => {
         return res.sendStatus(err.statusCode);
       }
 
+      throw err;
+    });
+});
+
+router.put("/change_password", (req, res) => {
+  const {
+    oldPassword: oldPlainTextPassword,
+    newPassword: newplaintextPassword,
+    newPasswordConfirmation: newplaintextPasswordConfirmation
+  } = req.body;
+
+  return comparePassword(oldPlainTextPassword, req.user.password)
+    .then(ok => {
+      if (!ok) {
+        throw { statusCode: 404 };
+      }
+
+      return req.user;
+    })
+    .then(() => {
+      if (newplaintextPassword !== newplaintextPasswordConfirmation) {
+        throw {statusCode: 400};
+      }
+    })
+    .then(() => hashPassword(newplaintextPassword))
+    .then(password => {
+      DB.run("UPDATE users SET password = ? WHERE id = ?", password, req.user.id);
+    })
+    .then(() => res.sendStatus(200))
+    .catch(err => {
+      if (err && err.statusCode) {
+        return res.sendStatus(err.statusCode);
+      }
       throw err;
     });
 });
