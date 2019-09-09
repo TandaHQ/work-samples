0a6351f39046b9f93cfb0602d9b93cf0a34d5a4a
Jon Phillips <jonphillips@Jons-MacBook-Pro.local>
2019-09-09 15:31:53 -0500

    handle create user

--- adnat (react)/frontend/src/App.js
+++ adnat (react)/frontend/src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="app-container">
-      <h1> Hello World! </h1>
-    </div>
-  );
+const baseAPI = `http://localhost:3000`
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: '',
+      userLoggedIn: false
+    }
+    this.handleCreateUser = this.handleCreateUser.bind(this)
+  }
+
+  handleCreateUser(user) {
+    console.log(user)
+    fetch(baseAPI + `/auth/signup`, {
+      body: JSON.stringify({user:user}),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(createdUser => createdUser.json())
+    .then(userJson => {
+      console.log(userJson)
+      this.setState({
+        userCreateMessage: 'User created successfully! Please Log-In to continue!',
+        loginError: ''
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      this.setState({
+        userCreateMessage: 'Error! User not created.',
+        loginError: ''
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <h1> Tanda. </h1>
+      </div>
+    )
+  }
 }
 
 export default App;
